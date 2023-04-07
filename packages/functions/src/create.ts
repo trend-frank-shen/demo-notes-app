66426d7dbc680c6bef25ad185faf89a2a0e0c77e
frank_shen <frank_shen@trendmicro.com>
2023-04-07 15:26:17 +0800

    feat: securing the API

--- packages/functions/src/create.ts
+++ packages/functions/src/create.ts
@@ -3,13 +3,13 @@ import * as uuid from 'uuid';
 import handler from '@notes/core/handler';
 import dynamoDb from '@notes/core/dynamodb';
 
-export const main = handler(async (event: { body: string }) => {
+export const main = handler(async (event:any) => {
   const data = JSON.parse(event.body);
   const params = {
     TableName: Table.Notes.tableName,
     Item: {
       // The attributes of the item to be created
-      userId: '123', // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId,
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
