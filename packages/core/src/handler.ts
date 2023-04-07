90a1324dd1b1db63d1a8e1cfefc3b500f81309fb
frank_shen <frank_shen@trendmicro.com>
2023-04-07 14:27:47 +0800

    feat: adding the API

---
+++ packages/core/src/handler.ts
@@ -0,0 +1,23 @@
+export default function handler(lambda: any) {
+  return async function (event: any, context: any) {
+    let body, statusCode;
+
+    try {
+      // Run the Lambda
+      body = await lambda(event, context);
+      statusCode = 200;
+    } catch (e: any) {
+      // Prints the full error
+      console.error(e);
+
+      body = { error: e.message };
+      statusCode = 500;
+    }
+
+    // Return HTTP response
+    return {
+      statusCode,
+      body: JSON.stringify(body),
+    };
+  };
+}
